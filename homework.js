290396f73535a2538e9ce01e50105932e1cfad14
kyrakan <102175506+kyrakan@users.noreply.github.com>
2022-04-03 18:00:37 -0400

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,30 @@
+alert(Date());
+function ftoc(f) {
+    return (5/9) * (f-32);
+}
+document.getElementById("demo").innerHTML = ftoc(20);
+function img(){
+    document.getElementById('hisoka').src='https://i.pinimg.com/originals/a3/c7/2b/a3c72b0d915c0727e18111e773547249.png'
+}
+function change(){
+	var select = document.getElementById('intro');
+  select.innerHTML ='Hello world';
+}
+function paragraph(){
+    var select = document.getElementById('change2');
+    select.style.backgroundColor='pink';
+    select.style.fontFamily='Arial';
+    select.style.textAlign='center';
+}
+function sum2(){
+    var a =parseFloat(prompt('first number:'));
+    var b= parseFloat(prompt('second number:'));
+    document.getElementById('sum').innerHTML=a+b;
+ }
+ function names(){
+
+ var c=prompt('please enter the first name:');
+ var d=prompt('please entger the second name');
+  document.getElementById('five').innerHTML="Nice to meet you"+" "+c+" "+d;
+}
+ 
